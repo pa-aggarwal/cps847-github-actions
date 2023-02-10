85bae9f9a512965d9a96154fb3d9846d25c8c454
Priya Aggarwal <51724388+pa-aggarwal@users.noreply.github.com>
2023-02-10 14:44:37 -0500

    Remove unused code and fix trailing whitespace

--- test_sample.py
+++ test_sample.py
@@ -3,13 +3,13 @@ Test helper functions
 """
 
 import unittest
-from sample import increment_by_two, increment_by_three
+from sample import increment_by_two
 
 class TestSampleMethods(unittest.TestCase):
     """
     Test harness
     """
-    
+
     def test_increment_by_two(self):
         """
         Test increments
@@ -17,7 +17,6 @@ class TestSampleMethods(unittest.TestCase):
         self.assertEqual(increment_by_two(-2), 0)
         self.assertEqual(increment_by_two(0), 2)
         self.assertEqual(increment_by_two(3), 5)
-        
+
 if __name__ == '__main__':
     unittest.main()
-    
