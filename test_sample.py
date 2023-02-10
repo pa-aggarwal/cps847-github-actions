8e29a3c8a26f12901428e0471e9a5723b846e5ff
Priya Aggarwal <51724388+pa-aggarwal@users.noreply.github.com>
2023-02-10 14:40:28 -0500

    Add unit test

---
+++ test_sample.py
@@ -0,0 +1,23 @@
+"""
+Test helper functions
+"""
+
+import unittest
+from sample import increment_by_two, increment_by_three
+
+class TestSampleMethods(unittest.TestCase):
+    """
+    Test harness
+    """
+    
+    def test_increment_by_two(self):
+        """
+        Test increments
+        """
+        self.assertEqual(increment_by_two(-2), 0)
+        self.assertEqual(increment_by_two(0), 2)
+        self.assertEqual(increment_by_two(3), 5)
+        
+if __name__ == '__main__':
+    unittest.main()
+    
