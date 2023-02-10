dcc894f9f0aff318c2138e251f51d2fb4ab360b5
Priya Aggarwal <51724388+pa-aggarwal@users.noreply.github.com>
2023-02-10 14:52:28 -0500

    Change business logic

--- sample.py
+++ sample.py
@@ -9,4 +9,4 @@ def increment_by_two(var_to_increment):
     :param var_to_increment: param to increment
     :return: incremented variable
     """
-    return var_to_increment + 2
+    return var_to_increment + 3
